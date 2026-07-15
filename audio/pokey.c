785e151af642d8740034707997c1b3d8a778aa5a
Hotlands Software <weatherisland3@gmail.com>
2026-07-15 13:19:56 -0400

    atari400: fix input bugs

--- audio/pokey.c
+++ audio/pokey.c
@@ -59,7 +59,7 @@ void pokey_write(Pokey *p, uint8_t reg, uint8_t v) {
     case 0x0D:                             /* SEROUT - no attached SIO target */
         p->serout = v;
         p->irq_pending &= (uint8_t)~POKEY_IRQ_SEROC;
-        p->irq_pending |= POKEY_IRQ_SEROR;
+        p->irq_pending |= (uint8_t)(POKEY_IRQ_SEROR & p->irqen);
         p->serout_delay = 4;
         break;
     case 0x0E:                             /* IRQEN: 0-bits also clear pending */
@@ -76,7 +76,7 @@ void pokey_tick(Pokey *p) {
      * and complete sources. Do not re-arm SEROR here; the OS has a real
      * vector for that source and will spin if it is held pending forever. */
     if (p->serout_delay && --p->serout_delay == 0)
-        p->irq_pending |= POKEY_IRQ_SEROC;
+        p->irq_pending |= (uint8_t)(POKEY_IRQ_SEROC & p->irqen);
 
     static const uint8_t irq_bits[3] = {
         POKEY_IRQ_TIMER1, POKEY_IRQ_TIMER2, POKEY_IRQ_TIMER4
@@ -84,7 +84,7 @@ void pokey_tick(Pokey *p) {
     for (int i = 0; i < 3; i++) {
         if (!p->timer_count[i]) p->timer_count[i] = timer_reload(p, i);
         if (--p->timer_count[i] == 0) {
-            p->irq_pending |= irq_bits[i];
+            p->irq_pending |= (uint8_t)(irq_bits[i] & p->irqen);
             p->timer_count[i] = timer_reload(p, i);
         }
     }
@@ -92,7 +92,7 @@ void pokey_tick(Pokey *p) {
 
 void pokey_key_down(Pokey *p, uint8_t code) {
     p->kbcode = code;
-    p->irq_pending |= POKEY_IRQ_KEY;
+    p->irq_pending |= (uint8_t)(POKEY_IRQ_KEY & p->irqen);
     p->skstat &= (uint8_t)~0x04;           /* key currently pressed */
     if (code & 0x40) p->skstat &= (uint8_t)~0x08;  /* shift held */
 }
@@ -102,5 +102,5 @@ void pokey_key_up(Pokey *p) {
 }
 
 void pokey_break_key(Pokey *p) {
-    p->irq_pending |= POKEY_IRQ_BREAK;
+    p->irq_pending |= (uint8_t)(POKEY_IRQ_BREAK & p->irqen);
 }
